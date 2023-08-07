560cf95f7c9fa35c5c3fee96affdaa8a09e00273
Muirey03 <tcmuir03@gmail.com>
2023-08-08 00:40:04 +0100

    Some linux versions don't define memmem?

--- src/heuristics.c
+++ src/heuristics.c
@@ -4,6 +4,31 @@
 #include "heuristics.h"
 #include "a64.h"
 
+#ifndef memmem
+void *memmem (const void *haystack, size_t hs_len, const void *needle, size_t ne_len) {
+    const char *hs = haystack;
+    const char *ne = needle;
+
+    if (ne_len == 0)
+        return (void *)hs;
+    int i;
+    int c = ne[0];
+    const char *end = hs + hs_len - ne_len;
+
+    for ( ; hs <= end; hs++)
+    {
+        if (hs[0] != c)
+            continue;
+        for (i = ne_len - 1; i != 0; i--)
+            if (hs[i] != ne[i])
+                break;
+        if (i == 0)
+            return (void *)hs;
+    }
+    return NULL;
+}
+#endif
+
 uint32_t g_externalMethod_idx = -1;
 uint32_t g_externalMethod2_idx = -1;
 uint32_t g_getTargetAndMethodForIndex_idx = -1;
